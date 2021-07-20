c7f89673ad1187aa8287cf77b6d3bcf11919530b
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2021-07-20 14:20:34 -0400

    Moved to nextjs, added under construction page

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head"
+import Image from "next/image"
+import styles from "../styles/Home.module.scss"
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Lucy Awrey's Personal Site</title>
+        <meta name="description" content="Lucy Awrey's personal site, it;s currently under construction." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          This site is currently under construction.
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home;
