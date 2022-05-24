646ec302810496a464ab90d32db713a4b4fa4427
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2022-05-24 01:27:19 -0400

    Created under construction banner and content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,14 +17,21 @@ function Home() {
             <Terminal model={terminal} />
             <p className="m-1 text-gray-500">{Text.terminalHelp}</p>
           </div>
-          <section className="bg-white shadow my-8 pb-4 container md:w-3/4 mx-auto">
+          <section className="bg-white shadow mt-8 mb-24 pb-4 container md:w-3/4 mx-auto text-xl text-center">
+          <header className="py-6 border-dashed border-b-2 border-gray-500 mb-4">
+            <h1 id="construction" className="text-3xl font-bold text-gray-900 text-center">👷‍♀️ Under Construction 👷‍♀️</h1>
+          </header>
+            <p className="px-12 py-6">{Text.construction}</p>
+          </section>
+          <section className="bg-white shadow my-8 pb-4 container md:w-3/4 mx-auto text-lg">
             <HeaderBar id="resume">Resume</HeaderBar>
             <p className="px-6 py-1">{Text.lorem1}</p>
             <p className="px-6 py-1">{Text.lorem2}</p>
           </section>
-          <section className="bg-white shadow my-8 pb-4 container md:w-3/4 mx-auto">
+          <section className="bg-white shadow my-8 pb-4 container md:w-3/4 mx-auto text-lg">
             <HeaderBar id="projects">Projects</HeaderBar>
             <p className="px-6 py-1">{Text.lorem3}</p>
+            <p className="px-6 py-1">{Text.lorem1}</p>
           </section>
         </main>
       </div>
