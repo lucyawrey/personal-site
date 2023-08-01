f189d0ea4119202be5aa40431fa718333df1b48f
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2023-07-31 20:27:03 -0700

    wip, does not work

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -14,7 +14,7 @@ function classs(...classes: any) {
 function NavMenu() {
   return (
     <Disclosure as="nav" class="bg-gray-800">
-      {({ open }) => (
+      {({ open }: any) => (
         <>
           <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex items-center justify-between h-16">
