39d405d3cb64c7fec2e2cfb0c80f422db430d6f9
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2023-07-31 18:33:33 -0700

    Added test api, changed to absolute imports

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,6 @@
 import { Head } from "$fresh/runtime.ts";
 import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+import Counter from "islands/Counter.tsx";
 
 export default function Home() {
   const count = useSignal(3);
