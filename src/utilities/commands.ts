82c1163abf35232bc127b8f6f41a8179bf3570e3
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2025-01-04 14:54:53 -0500

    added link command

--- src/utilities/commands.ts
+++ src/utilities/commands.ts
@@ -1,10 +1,21 @@
 import { TerminalModel } from "models/TerminalModel";
 import Text from "content/text.json";
 import GameScript from "content/game-script.txt";
-import { jump } from "utilities/helpers";
+import { isClient, jump } from "utilities/helpers";
 
 function help(args: string[], terminal: TerminalModel) {
   terminal.print(Text.helpCommand);
+}
+
+function link(args: string[], terminal: TerminalModel) {
+  if (args[0] && (args[0] === "linkedin" || args[0] === "github")) {
+    const link = Text.links[args[0]];
+    if (isClient()) {
+      window.open(link, "_blank")?.focus();
+    }
+  } else {
+    terminal.print(Text.errorLinkArgument);
+  }
 }
 
 function go(args: string[], terminal: TerminalModel) {
@@ -28,5 +39,5 @@ function cute(args: string[], terminal: TerminalModel) {
   terminal.print(Text.cute);
 }
 
-const Commands: any = { help, go, clear, game, cute };
+const Commands: any = { help, link, go, clear, game, cute };
 export default Commands;
